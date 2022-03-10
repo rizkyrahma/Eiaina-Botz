fff8ca13959b76d0bd438a46da24b7baafbb20f4
Nur Alam <99713260+rizkyrahma@users.noreply.github.com>
2022-03-10 15:55:19 +0700

    Update sourcecode.js

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -1,10 +1,10 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'Source code')).buffer(), `
 *────────「 SCRIPT 」 ────────*
-Script Ori: https://github.com/Kangsad01/wabot-tsb01
+Script Ori: https://github.com/rizkyrahma
 
-Script Recode By Pilar: https://github.com/PilarV2/Zeus-BotzV4
-`.trim(), '© ZeusBotz', 'Thanks', '.tqto')
+Script Recode By Pilar: https://github.com/rizkyrahma/Eiaina-Botz
+`.trim(), '© AlamBotz', 'Thanks', '.tqto')
 handler.help = ['sourcecode']
 handler.tags = ['info']
 handler.command = /^(sourcecode|sc|scbot|script|github)$/i
